a4c7f37525f761cd27e2e0654ebc276d8a387bcd
Buddy <jangtaehwan2@gmail.com>
2022-03-30 09:27:01 +0900

    Project Created

---
+++ src/test/java/com/tutoring/tutoring/TutoringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutoring.tutoring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutoringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

2429f328ec1c36cda4bd3939397379eaa8c04ca8
Buddy <jangtaehwan2@gmail.com>
2022-05-11 18:45:27 +0900

    Ver 20220511

--- src/main/java/com/tutoring/tutoring/TutoringApplication.java
+++ src/main/java/com/tutoring/tutoring/TutoringApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TutoringApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Ver.20220511");
 		SpringApplication.run(TutoringApplication.class, args);
 	}
 }
